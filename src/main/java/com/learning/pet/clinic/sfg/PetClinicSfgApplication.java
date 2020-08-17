535dc0fcb1fa5988090e0fc357f7d7dbf56de8cd
aditya <adityaajmera89@gmail.com>
2020-08-17 14:44:55 +0530

    Initial project setup. Closes #1

---
+++ src/main/java/com/learning/pet/clinic/sfg/PetClinicSfgApplication.java
@@ -0,0 +1,13 @@
+package com.learning.pet.clinic.sfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicSfgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicSfgApplication.class, args);
+	}
+
+}
