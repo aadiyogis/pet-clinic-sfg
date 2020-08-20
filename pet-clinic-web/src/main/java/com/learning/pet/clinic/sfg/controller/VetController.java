d47a6d2535ce031158cc22f208869c91a6b4f0b1
adiyogis <adiyogisiddha@gmail.com>
2020-08-20 11:11:12 +0530

    Displaying list of vets. Closes #14

--- pet-clinic-web/src/main/java/com/learning/pet/clinic/sfg/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/learning/pet/clinic/sfg/controller/VetController.java
@@ -1,6 +1,8 @@
 package com.learning.pet.clinic.sfg.controller;
 
+import com.learning.pet.clinic.sfg.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,8 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/vet")
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String vetIndex() {
+    public String vetIndex(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
